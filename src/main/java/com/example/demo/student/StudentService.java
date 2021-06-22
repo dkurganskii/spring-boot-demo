45d050f87e5cf2e0c1e2a9b6700d76393c91543d
dkurganskii <44755635+dkurganskii@users.noreply.github.com>
2021-06-22 13:39:38 -0500

    implemetn JPA

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,6 +1,6 @@
 package com.example.demo.student;
 
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -9,11 +9,15 @@ import java.util.List;
 
 @Service
 public class StudentService {
-    public List<Student> getStudents(){
-        return List.of(new Student(
-                1L, "Mariam",
-                "mariam.jamal@gmail.com",
-                LocalDate.of(2000, Month.JANUARY, 5),
-                21));
+
+    @Autowired
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public List<Student> getStudents() {
+       return studentRepository.findAll();
     }
 }
